8b7223d8c6fa23022861efcc9a4e7b2a1d272baf
Joseph Sinnott <joseph.sinnott@student.manchester.ac.uk>
2024-11-10 17:05:42 +0000

    created and implemented gravity function

--- game_source.py
+++ game_source.py
@@ -5,6 +5,12 @@ class View(tk.Frame):
     def __init__(self, root):
         tk.Frame.__init__(self, root)
 
+        self.acceleration = 0.0007
+        self.speed = 0
+        self.character_x = 0.5
+        self.character_y = 0.5
+
+
         # Load and scale the image using Pillow
         original_image = Image.open("comp16321-labs_y46354js/images/test.png")
         new_width = int(original_image.width * 4)
@@ -15,10 +21,25 @@ class View(tk.Frame):
         self.photo = ImageTk.PhotoImage(scaled_image)
 
         # Create a label to hold the scaled image
-        self.image_label = tk.Label(self, image=self.photo)
-        self.image_label.pack()
+        self.character = tk.Label(self, image=self.photo)
+        self.character.pack()
 
-if __name__ == '__main__':
+    def gravity(self, event=None): # Moves the character to simulate the effects of gravity
+        
+        # Apply the speed and acceleration variables
+        self.speed += self.acceleration
+        self.character_y += self.speed
+
+        # Stop character falling if touching bottom of screen
+        if self.character_y >= 0.88:
+            self.character_y = 0.88
+        self.character.place(relx=self.character_x, rely=self.character_y, anchor='center')
+
+
+        # Call function every 10 ms
+        root.after(10, self.gravity)
+
+if __name__ == '__main__': # Runs if this file is ran directly
     root = tk.Tk()
 
     # Set window dimensions
@@ -29,5 +50,7 @@ if __name__ == '__main__':
     # Create View instance
     view = View(root)
     view.pack(side="top", fill="both", expand=True)
+
+    view.gravity()
 
     root.mainloop()
