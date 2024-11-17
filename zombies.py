71351ae9517fcc3e2eecf13eb8de5738507dcb7b
Joseph Sinnott <joseph.sinnott@student.manchester.ac.uk>
2024-11-17 01:51:58 +0000

    created and implemented function for moving the zombies towards the centre

--- zombies.py
+++ zombies.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from random import randint
 
-class zombie():
+class zombie:
     def __init__(self, canvas):
 
         # Load and scale the zombie image
@@ -13,11 +13,19 @@ class zombie():
         zombie_image = zombie_image.resize((new_width, new_height), Image.NEAREST)
         self.zombie_tk_image = ImageTk.PhotoImage(zombie_image)
 
-        self.speed = 0
+        self.speed_x = 0
+        self.speed_y = 0
+        self.x = randint(5,95)/100
+        self.y = -0.1
 
         self.image = canvas.create_image(
-            randint(0,100)/100 * canvas.width,
-            -0.1 * canvas.height,
+            self.x * canvas.width,
+            self.y * canvas.height,
             image=self.zombie_tk_image,
-            anchor="center"
+            anchor=tk.S
         )
+    def movement(self):
+        if self.x >= 0.5:
+            self.speed_x = -0.001
+        else:
+            self.speed_x = 0.001
